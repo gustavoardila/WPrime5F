87315f88131261b13738dcc51412946a9d9e9b7b
Gustavo Ardila <ga.ardila10@uniandes.edu.co>
2021-10-14 06:39:50 -0500

    UFO added

---
+++ WPrime5F+SM_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.33
+# Mathematica version: 12.0.0 for Mac OS X x86 (64-bit) (April 7, 2019)
+# Date: Thu 14 Oct 2021 06:27:30
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ WPrime5F+SM_UFO/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.33
+# Mathematica version: 12.0.0 for Mac OS X x86 (64-bit) (April 7, 2019)
+# Date: Thu 14 Oct 2021 06:27:30
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+BSM = CouplingOrder(name = 'BSM',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
